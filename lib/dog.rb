2f2636cc2f5841de64c87551a583d1caeaa1b313
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-22 03:47:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@ class Dog
    def name=(first_name)
      @name= first_name
     end
-  def name
-    @name
-  end
-  def breed=(type)
-    @breed= type
-   end
-  def breed
-   @breed
-  end
+    def name
+      @name
+    end
+    def breed=(type)
+      @breed= type
+     end
+    def breed
+     @breed
+    end
 
 
 end
